d216a6039b4edef917123961675b06018140c8d0
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-03-26 11:31:16 -0700

    Add project files.

---
+++ Listas/Pilas.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listas
+{
+    public class Pilas
+    {
+        private List<string> lista;
+        public Pilas()
+        {
+            lista = new List<string>();
+        }
+
+        public void Agregar(string dato) 
+        {
+            lista.Add(dato);
+        }
+
+        private bool ValidaVacio() 
+        {
+            //Regresa bool con if
+            //if (lista.Count == 0) 
+            //{
+            //    return true;
+            //}
+            //else 
+            //{
+            //    return false;
+            //}
+            
+            //Regresa bool con un operador ternario
+            //return (lista.Count == 0) ? true : false;
+
+            //If es una operacion booleana,
+            //si la condicion se cumple, regresa true
+            //si no se cumple regresa false
+            //por lo tanto, se puede simplificar de la siguiente manera
+            return (lista.Count == 0);
+        }
+        
+        public void Eliminar() 
+        {
+            if (ValidaVacio()) 
+            {
+                throw new Exception("Lista vacía");
+            }
+
+            lista.RemoveAt(lista.Count - 1);
+        }
+
+        public string Imprimir() 
+        {
+            string datos = string.Empty;
+
+            if (ValidaVacio()) 
+            {
+                return "Lista vacía";
+            }
+
+            int lstCount = lista.Count;
+            for (int i = 0; i < lstCount; i++) 
+            {
+                if (i > 0) 
+                {
+                    datos += "\n";
+                }
+
+                datos += $"[{i}] - {lista[i]}";
+            }
+
+            return datos;
+        }
+    }
+}
---
+++ PilasColas-master/PilasColas/Program.cs
@@ -0,0 +1,224 @@
+﻿using Arreglos;
+using Listas;
+using System;
+
+namespace PilasColas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListasColas();
+        }
+
+        static void ArregloPilas() 
+        {
+            try
+            {
+                Arreglos.Pilas Arraypila = new Arreglos.Pilas(5);
+
+                //pila.Agregar("Cero");
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Agregar("Uno");
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Agregar("Dos");
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Agregar("Tres");
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Agregar("Cuatro");
+                //Console.WriteLine(pila.Imprimir());
+
+
+                //Console.WriteLine("---------");
+                //pila.Eliminar();
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Eliminar();
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Eliminar();
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Eliminar();
+                //Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Eliminar();
+                //Console.WriteLine(pila.Imprimir());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void ArregloColas() 
+        {
+            try
+            {
+                Arreglos.Colas colas = new Arreglos.Colas(5);
+
+                Console.WriteLine(colas.Imprimir());
+                colas.Agregar("Cero");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Uno");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Dos");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Tres");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Cuatro");
+                Console.WriteLine(colas.Imprimir());
+
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ListasPilas()
+        {
+            try
+            {
+                Listas.Pilas pila = new Listas.Pilas();
+
+                pila.Agregar("Cero");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Uno");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Dos");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Tres");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Cuatro");
+                Console.WriteLine(pila.Imprimir());
+
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void ListasColas()
+        {
+            try
+            {
+                Listas.Colas colas = new Listas.Colas();
+
+                Console.WriteLine(colas.Imprimir());
+                colas.Agregar("Cero");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Uno");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Dos");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Tres");
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Agregar("Cuatro");
+                Console.WriteLine(colas.Imprimir());
+
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+                Console.WriteLine("---------");
+                colas.Eliminar();
+                Console.WriteLine(colas.Imprimir());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
